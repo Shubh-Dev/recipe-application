bd51148e9160597d55fbef67b7df5a725a1fa1bd
Shubh <shubh.scb@gmail.com>
2022-11-07 17:37:36 +0530

    USERADD

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, Only: [:index]
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'users#index'
 end
