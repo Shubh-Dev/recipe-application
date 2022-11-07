b729e10f0a9da22f6d73ff07ee81bd77ee7b12d6
Shubh <shubh.scb@gmail.com>
2022-11-07 19:24:49 +0530

    LINTERS INSTALL

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,8 +1,8 @@
 class Food < ApplicationRecord
-    belongs_to :User
-    has_many :recipe_foods, dependent: :destroy
+  belongs_to :User
+  has_many :recipe_foods, dependent: :destroy
 
-    validates :name, presence: true
-    validates :price, presence: true, numericality: { greater_than: 0 }
-    validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :name, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
 end
