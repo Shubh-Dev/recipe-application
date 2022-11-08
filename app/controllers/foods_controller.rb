f6670631ca5b32c08f3ae0d7899ee6777db5a21a
Shubh <shubh.scb@gmail.com>
2022-11-08 19:29:06 +0530

    LINTERS ERR FIX

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,21 +3,22 @@ class FoodsController < ApplicationController
     @foods = current_user.foods.all
   end
 
-    def new
-        @food = Food.new
-    end
+  def new
+    @food = Food.new
+  end
 
-    def create
-        @food = current_user.foods.new(food_params)
-        if @food.save
-            redirect_to foods_path
-        else
-            render :new
-        end
+  def create
+    @food = current_user.foods.new(food_params)
+    if @food.save
+      redirect_to foods_path
+    else
+      render :new
     end
-    private 
+  end
 
-    def food_params
-        params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
-    end
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
