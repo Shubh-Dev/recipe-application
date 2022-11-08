cc0fdef8927615c065f2f6f5422289e2578881f5
Shubh <shubh.scb@gmail.com>
2022-11-08 23:31:14 +0530

    LINTE ERR FIX

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -4,36 +4,35 @@ RSpec.describe Food, type: :model do
   subject { Food.create(name: 'FOOD1', price: 3, measurement_unit: 'kilos', quantity: 3, user_id: 1) }
   before { subject.save }
 
-    it 'name should be present' do
-     subject.name = ''
-     expect(subject).to_not be_valid
-    end
+  it 'name should be present' do
+    subject.name = ''
+    expect(subject).to_not be_valid
+  end
 
-    it 'price should be present' do
-        subject.price = ''
-        expect(subject).to_not be_valid
-        end
+  it 'price should be present' do
+    subject.price = ''
+    expect(subject).to_not be_valid
+  end
 
-    it 'measurement_unit should be present' do
-        subject.measurement_unit = ''
-        expect(subject).to_not be_valid
-        end
-    
-    it 'quantity should be present' do
-        subject.quantity = ''
-        expect(subject).to_not be_valid
-        end
-    it 'price should be a number' do
-        subject.price = 'a'
-        expect(subject).to_not be_valid
-        end
-    it 'quantity should be a number' do
-        subject.quantity = 'a'
-        expect(subject).to_not be_valid
-        end
-    it 'price should be greater than 0' do
-        subject.price = 0
-        expect(subject).to_not be_valid
-        end
+  it 'measurement_unit should be present' do
+    subject.measurement_unit = ''
+    expect(subject).to_not be_valid
+  end
 
+  it 'quantity should be present' do
+    subject.quantity = ''
+    expect(subject).to_not be_valid
+  end
+  it 'price should be a number' do
+    subject.price = 'a'
+    expect(subject).to_not be_valid
+  end
+  it 'quantity should be a number' do
+    subject.quantity = 'a'
+    expect(subject).to_not be_valid
+  end
+  it 'price should be greater than 0' do
+    subject.price = 0
+    expect(subject).to_not be_valid
+  end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,53 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-    subject do
-        Recipe.new(name: 'Recipe', preparation_time: 20, cooking_time: 15, description: 'text', public: true, user_id: 1)
-      end
-      before { subject.save }
+  subject do
+    Recipe.new(name: 'Recipe', preparation_time: 20, cooking_time: 15, description: 'text', public: true,
+               user_id: 1)
+  end
+  before { subject.save }
 
-    it 'name should be present' do
-        subject.name = ''
-        expect(subject).to_not be_valid
-    end
+  it 'name should be present' do
+    subject.name = ''
+    expect(subject).to_not be_valid
+  end
 
-    it 'preparation_time should be present' do
-        subject.preparation_time = ''
-        expect(subject).to_not be_valid
-    end
+  it 'preparation_time should be present' do
+    subject.preparation_time = ''
+    expect(subject).to_not be_valid
+  end
 
-    it 'cooking_time should be present' do
-        subject.cooking_time = ''
-        expect(subject).to_not be_valid
-    end 
+  it 'cooking_time should be present' do
+    subject.cooking_time = ''
+    expect(subject).to_not be_valid
+  end
 
-    it 'description should be present' do
-        subject.description = ''
-        expect(subject).to_not be_valid
-    end
+  it 'description should be present' do
+    subject.description = ''
+    expect(subject).to_not be_valid
+  end
 
-    it 'public should be present' do
-        subject.public = ''
-        expect(subject).to_not be_valid
-    end
+  it 'public should be present' do
+    subject.public = ''
+    expect(subject).to_not be_valid
+  end
 
-    it 'preparation_time should be a number' do
-        subject.preparation_time = 'a'
-        expect(subject).to_not be_valid
-    end
+  it 'preparation_time should be a number' do
+    subject.preparation_time = 'a'
+    expect(subject).to_not be_valid
+  end
 
-    it 'cooking_time should be a number' do
-        subject.cooking_time = 'a'
-        expect(subject).to_not be_valid
-    end
+  it 'cooking_time should be a number' do
+    subject.cooking_time = 'a'
+    expect(subject).to_not be_valid
+  end
 
-    it 'preparation_time should be greater than 0' do
-        subject.preparation_time = 0
-        expect(subject).to_not be_valid
-    end
+  it 'preparation_time should be greater than 0' do
+    subject.preparation_time = 0
+    expect(subject).to_not be_valid
+  end
 
-    it 'cooking_time should be greater than 0' do
-        subject.cooking_time = 0
-        expect(subject).to_not be_valid
-    end
+  it 'cooking_time should be greater than 0' do
+    subject.cooking_time = 0
+    expect(subject).to_not be_valid
+  end
 end
